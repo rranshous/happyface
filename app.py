0e87b020d05bf3907e412b6cb9dd462be4d60831
Robby Ranshous <rranshous@gmail.com>
2023-07-30 15:20:12 -0400

    also look for faces

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sys
 
 # Load the cascade
 smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface.xml")
 
 # load the image
 image_path = sys.argv.pop(1)
@@ -15,13 +16,19 @@ image = cv2.imread(image_path)
 # Convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# detect for smile
-smile = smile_cascade.detectMultiScale(gray, scaleFactor=1.8, minNeighbors=20)
+# detect for face
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
 
-# output success / failure
-if smile.any():
-    sys.stdout.write("smile found")
-    exit(0)
-else:
-    sys.stdout.write("smile not found")
-    exit(1)
+# no faces detected
+if len(faces) == 0:
+  exit(1)
+
+# detect for smile
+smiles = smile_cascade.detectMultiScale(gray, scaleFactor=1.8, minNeighbors=20)
+
+# no smile detected
+if len(smiles) == 0:
+  exit(1)
+
+# we must have found faces and smiles!
+exit(0)
